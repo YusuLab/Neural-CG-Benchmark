832dc6cb1fae6145e93f95389a05dac72ca9fe39
chens5 <srchen98@gmail.com>
2026-09-08 10:58:01 -0700

    add eps-kernel task

--- nugets/models/encoder_decoders/epsilon_kernel.py
+++ nugets/models/encoder_decoders/epsilon_kernel.py
@@ -9,7 +9,7 @@ import nugets.losses.losses as Losses
 from nugets.datasets.datapoint_types import Set_batch, Point_datapoint, LabeledSetBatch
 from nugets.models.model import EncoderDecoder
 
-class SetIdentityEncoderDecoder(EncoderDecoder):
+class EpsilonKernelIdentityEncoderDecoder(EncoderDecoder):
     def __init__(self, input_dim: int, backbone_input_dim: int, backbone_output_dim: int, output_dim: int|None,
                 loss_function:str, absolute_positional_encoding: str | None = None, inject_noise: float = 0.1, *args, **kwargs):
         super().__init__()
@@ -19,8 +19,10 @@ class SetIdentityEncoderDecoder(EncoderDecoder):
     
     def encoder(self, batch: Set_batch):
         return batch, None
+    
     def decode(self, result: Set_batch):
         return result
+    
     def compute_result(self, batch: Set_batch, backbone_result: Set_batch, encoder_info):
         eps_kernel = self.decode(backbone_result)
         return self.loss_function(predicted=eps_kernel, target=batch, in_dim=self.input_dim)
--- nugets/tasks/epsilon_kernel_task.py
+++ nugets/tasks/epsilon_kernel_task.py
@@ -12,6 +12,8 @@ from torch import Tensor
 from torch_heterogeneous_batching import Batch
 from nugets.datasets.datapoint_types import Set_batch, Set_datapoint
 from nugets.models.backbone import BackBone
+import warnings
+
 
 from .task import Task
 from .register import register
@@ -25,5 +27,16 @@ class EpsilonKernelTask(Task):
     def datapoint_type(self):
         return Set_datapoint
 
-    def get_encoder_decoder(self, backbone:BackBone, loss_function: str, **kwargs):
-        raise NotImplementedError("Not done yet")
+    def get_encoder_decoder(self, backbone:BackBone, loss_function: str="direction_width_loss", **kwargs):
+
+        from nugets.models.encoder_decoders.epsilon_kernel import EpsilonKernelIdentityEncoderDecoder
+        dataset_info = self.dataset_info()
+        backbone_input_dim = dataset_info["dim"]
+        backbone_output_dim = dataset_info["dim"]
+        if loss_function != "epsilon_kernel_projection_loss":
+            warnings.warn("Only direction_width_loss compatible with this task.")
+        return EpsilonKernelIdentityEncoderDecoder(input_dim=dataset_info["dim"],
+                                                   backbone_input_dim=backbone_input_dim,
+                                                   backbone_output_dim=backbone_output_dim,
+                                                   output_dim = dataset_info["dim"],
+                                                   loss_function="direction_width_loss")
